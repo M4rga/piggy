cc85a34191c3f22c186fbb1b4626f9d3f0122cc6
M4rga <119053214+M4rga@users.noreply.github.com>
2024-07-25 17:58:10 +0200

    add-nativewind

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,20 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Viva i negriii</Text>
-      <StatusBar style="auto" />
+    <View className=" flex-1 justify-center items-center ">
+      <Text>Ciaooooo</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
