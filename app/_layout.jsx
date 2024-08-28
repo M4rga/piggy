50ea81ed8b5aa70d333d87eb499dd617a53fc0a4
M4rga <119053214+M4rga@users.noreply.github.com>
2024-08-28 11:38:14 +0200

    navbar 2.0

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,22 +1,60 @@
-import { StyleSheet, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "./home";
 import Stats from "./stats";
 import Wallet from "./wallet";
-import Navbar from "../components/Navbar/navbar";
+import Icon from "react-native-vector-icons/Feather";
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer independent={true}>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Wallet" component={Wallet} />
-        <Stack.Screen name="Stats" component={Stats} />
-      </Stack.Navigator>
-      <Navbar />
+      <Tab.Navigator
+        screenOptions={{
+          tabBarShowLabel: false,
+        }}
+      >
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Icon
+                name="home"
+                size={22}
+                color={focused ? "#F773ED" : "black"}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Wallet"
+          component={Wallet}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Icon
+                name="inbox"
+                size={24}
+                color={focused ? "#F773ED" : "black"}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Stats"
+          component={Stats}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Icon
+                name="bar-chart"
+                size={24}
+                color={focused ? "#F773ED" : "black"}
+              />
+            ),
+          }}
+        />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
