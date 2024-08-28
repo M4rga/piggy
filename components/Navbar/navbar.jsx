5821279812699111d35b45a5850c66e9b8ecdaca
M4rga <119053214+M4rga@users.noreply.github.com>
2024-08-28 10:49:33 +0200

    navbar 1.1

--- components/Navbar/navbar.jsx
+++ components/Navbar/navbar.jsx
@@ -1,18 +1,35 @@
+import { useState } from "react";
 import { View, StyleSheet, Button } from "react-native";
 import Icon from "react-native-vector-icons/Feather";
 import { useNavigation } from "@react-navigation/native";
 
 export default function Navbar() {
   const navigation = useNavigation();
+  const [home, setHomeColor] = useState("#F773ED");
+  const [stats, setStatsColor] = useState("black");
   return (
     <View style={styles.container}>
-      <Icon name="home" size={22} onPress={() => navigation.navigate("Home")} />
+      <Icon
+        name="home"
+        size={22}
+        onPress={() => {
+          navigation.navigate("Home");
+          setHomeColor("#F773ED");
+          setStatsColor("black");
+        }}
+        color={home}
+      />
       <Icon name="inbox" size={24} />
       <Icon name="plus" size={28} style={{ marginTop: -1.5 }} />
       <Icon
         name="bar-chart"
         size={24}
-        onPress={() => navigation.navigate("Stats")}
+        onPress={() => {
+          navigation.navigate("Stats");
+          setHomeColor("black");
+          setStatsColor("#F773ED");
+        }}
+        color={stats}
       />
       <Icon name="lock" size={24} />
     </View>
@@ -21,6 +38,8 @@ export default function Navbar() {
 
 const styles = StyleSheet.create({
   container: {
+    shadowOffset: { width: 0, height: -6 },
+    shadowOpacity: 0.05,
     backgroundColor: "white",
     flexDirection: "row",
     justifyContent: "space-around",
