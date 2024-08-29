b3850819bbbc4b240af4b50a97a20a266e7d428d
JacopoNicolini <jacopo.nicolini05@gmail.com>
2024-08-29 11:55:39 +0200

    V3

--- app/home.jsx
+++ app/home.jsx
@@ -4,17 +4,19 @@ import { useState, useEffect } from "react";
 import * as Font from "expo-font";
 // -------------------------
 import { View, StyleSheet, Image, ScrollView } from "react-native";
-import Icon from "react-native-vector-icons/Feather"; // Import Feather icons
-import Icon2 from "react-native-vector-icons/FontAwesome"; // Import Feather icons
-import Icon3 from "react-native-vector-icons/MaterialCommunityIcons"; // Import Feather icons
+import Icon from "react-native-vector-icons/Feather"; 
+import Icon2 from "react-native-vector-icons/FontAwesome"; 
+import Icon3 from "react-native-vector-icons/MaterialCommunityIcons"; 
 
 const pig_empty = require("../assets/homepage/Pig_empty.png");
+const pig_mid = require("../assets/homepage/Pig_mid.png");
+const pig_full = require("../assets/homepage/Pig_full.png");
 const pig_logo = require("../assets/homepage/Pig_logo.png");
 const menu = require("../assets/homepage/Menu_home.png");
 
 export default function Home() {
   // ------------FONT---------------
-  const [fontsLoaded, setFontsLoaded] = useState(false); // Correct useState import
+  const [fontsLoaded, setFontsLoaded] = useState(false);
   const loadFonts = () => {
     return Font.loadAsync({
       "Switzer-Variable": require("../assets/font/Switzer-Variable.ttf"),
@@ -30,70 +32,113 @@ export default function Home() {
   }, []);
 
   if (!fontsLoaded) {
-    return null; // Or show a loading indicator
+    return null;
   }
   // -------------------------
+  // Function to return dynamic card styles
+  const getCardStyle = (iconType) => {
+    switch (iconType) {
+      case "credit-card":
+        return { 
+          backgroundColor: "#ECE9EA", // Gray
+          textColor: "#000000", //Black Text
+        }; 
+      case "paypal":
+        return { 
+          backgroundColor: "#5272F2", // Blue
+          textColor: "#FFFFFF", // White text
+        }; 
+      case "cash":
+        return { 
+          backgroundColor: "#2F212F", // Dark
+          textColor: "#FFFFFF", // White text
+        }; 
+      default:
+        return { 
+          backgroundColor: "#F773ED", // Default Pink
+          textColor: "#FFFFFF", // White text
+        }; 
+    }
+  };
+
+  const getPigImage = (balance) => {
+    if(balance < 500) {
+      return pig_empty;
+    } else if(balance >= 500 && balance < 10000) {
+      return pig_mid;
+    } else {
+      return pig_full;
+    }
+  }
+
+  const totalBalance = 20275.78;
+
+  const formatBalance = (balance) => {
+    const [euros, cents] = balance.toFixed(2).split(".");
+    return { euros, cents };
+  };
+
+  const { euros, cents } = formatBalance(totalBalance);
+
   return (
     <ScrollView style={styles.container}>
-      {/* Container for the content within the scroll view */}
       <View style={styles.contentContainer}>
-        {/* View for the top section with an image */}
         <View style={styles.topView}>
-          <Image source={pig_empty} style={styles.pig_top} />
+          <Image source={getPigImage(totalBalance)} style={styles.pig_top} />
         </View>
 
-        {/* Main content area */}
         <View style={styles.mainContainer}>
-          {/* Row container for logo, text, and menu icon */}
           <View style={styles.row}>
-            {/* Wrapper View to create a pink circle around the pig_logo */}
             <View style={styles.logoContainer}>
               <Image source={pig_logo} style={styles.pig_logo} />
             </View>
-            {/* Text greeting the user */}
             <Text style={styles.text}>Ciao Marco!</Text>
 
-            {/* Flex View to push the menu icon to the right */}
             <View style={{ flex: 1 }}></View>
 
-            {/* Menu icon aligned to the right */}
             <Image source={menu} style={styles.menuHome} />
           </View>
 
-          {/* Additional content below */}
           <View style={styles.balanceContainer}>
-            {/* Wrap all text content inside a <Text> component */}
             <Text style={styles.totalBalanceText}>Bilancio Totale</Text>
             <Text style={styles.totalBalanceAmount}>
-              € 20.275<Text style={styles.totalBalanceDecimal}>.78</Text>
+              € {euros}<Text style={styles.totalBalanceDecimal}>.{cents}</Text>
             </Text>
 
-            {/* Card section with icon and amount */}
             <View style={styles.allCards}>
-              <View style={styles.card}>
-                <View style={styles.cardAmountRow}>
-                  <Text style={styles.cardAmount}>€ 6.579</Text>
-                  <Text style={styles.cardAmountDecimal}>,78</Text>
-                  <Icon name="credit-card" size={12} color="#000" />
+              <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+                <View style={[styles.card, { backgroundColor: getCardStyle("credit-card").backgroundColor }]}>
+                  <Icon name="credit-card" size={12} color={getCardStyle("credit-card").textColor} style={styles.cardIcon} />
+                  <View style={styles.cardAmountRow}>
+                    <Text style={[styles.cardAmount, { color: getCardStyle("credit-card").textColor }]}>€ 6.579</Text>
+                    <Text style={[styles.cardAmountDecimal, { color: getCardStyle("credit-card").textColor }]}>,78</Text>
+                  </View>
                 </View>
-              </View>
 
-              {/* Repeat card structure as needed */}
-              <View style={styles.card}>
-                <View style={styles.cardAmountRow}>
-                  <Text style={styles.cardAmount}>€ 6.579</Text>
-                  <Text style={styles.cardAmountDecimal}>,78</Text>
-                  <Icon2 name="paypal" size={12} color="#000" />
+                <View style={[styles.card, { backgroundColor: getCardStyle("paypal").backgroundColor }]}>
+                  <Icon2 name="paypal" size={12} color={getCardStyle("paypal").textColor} style={styles.cardIcon} />
+                  <View style={styles.cardAmountRow}>
+                    <Text style={[styles.cardAmount, { color: getCardStyle("paypal").textColor }]}>€ 62</Text>
+                    <Text style={[styles.cardAmountDecimal, { color: getCardStyle("paypal").textColor }]}></Text>
+                  </View>
                 </View>
-              </View>
 
-              <View style={styles.card}>
-                <View style={styles.cardAmountRow}>
-                  <Text style={styles.cardAmount}>€ 6.579</Text>
-                  <Text style={styles.cardAmountDecimal}>,78</Text>
-                  <Icon3 name="cash" size={12} color="#000" />
+                <View style={[styles.card, { backgroundColor: getCardStyle("cash").backgroundColor }]}>
+                  <Icon3 name="cash" size={12} color={getCardStyle("cash").textColor} style={styles.cardIcon} />
+                  <View style={styles.cardAmountRow}>
+                    <Text style={[styles.cardAmount, { color: getCardStyle("cash").textColor }]}>€ 834</Text>
+                    <Text style={[styles.cardAmountDecimal, { color: getCardStyle("cash").textColor }]}></Text>
+                  </View>
                 </View>
-              </View>
+
+                <View style={[styles.card, { backgroundColor: getCardStyle("cash").backgroundColor }]}>
+                  <Icon3 name="cash" size={12} color={getCardStyle("cash").textColor} style={styles.cardIcon} />
+                  <View style={styles.cardAmountRow}>
+                    <Text style={[styles.cardAmount, { color: getCardStyle("cash").textColor }]}>€ 12.800</Text>
+                    <Text style={[styles.cardAmountDecimal, { color: getCardStyle("cash").textColor }]}>,78</Text>
+                  </View>
+                </View>
+              </ScrollView>
             </View>
           </View>
         </View>
@@ -104,62 +149,62 @@ export default function Home() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, // Ensures the ScrollView takes the full height of the screen
+    flex: 1,
   },
   contentContainer: {
-    flexGrow: 1, // Allows the content to expand to fill the ScrollView
-    justifyContent: "flex-start", // Aligns content to the top of the ScrollView
+    flexGrow: 1,
+    justifyContent: "flex-start",
   },
   topView: {
-    alignItems: "center", // Centers the image horizontally
-    justifyContent: "center", // Centers the image vertically
-    paddingTop: 50, // Adds padding to the top of the view
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 50,
   },
   mainContainer: {
-    flex: 1, // Makes this container take up remaining space
-    backgroundColor: "#FFFFFF", // White background color for main container
-    borderTopLeftRadius: 40, // Rounded corners on the top left
-    borderTopRightRadius: 40, // Rounded corners on the top right
-    paddingHorizontal: 2, // Add horizontal padding inside main container
+    flex: 1,
+    backgroundColor: "#FFFFFF",
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40,
+    paddingHorizontal: 2,
   },
   pig_top: {
     flex: 1,
-    width: 230, // Width of the pig image
-    height: 189, // Height of the pig image
-    marginBottom: -16, // Negative margin to overlap the image
+    width: 230,
+    height: 189,
+    marginBottom: -16,
   },
   pig_logo: {
-    width: 48, // Width of the logo image
-    height: 33, // Height of the logo image
+    width: 48,
+    height: 33,
   },
   row: {
-    flexDirection: "row", // Aligns children in a horizontal row
-    alignItems: "center", // Centers children vertically within the row
-    paddingLeft: 20, // Adds left padding to the row
-    paddingTop: 15, // Adds top padding to the row
-    justifyContent: "space-between", // Distributes space between elements in the row
-    paddingRight: 20, // Adds right padding to the row
+    flexDirection: "row",
+    alignItems: "center",
+    paddingLeft: 20,
+    paddingTop: 15,
+    justifyContent: "space-between",
+    paddingRight: 20,
   },
   text: {
-    fontSize: 23, // Font size for the greeting text
+    fontSize: 23,
     fontFamily: "Switzer-Semibold",
   },
   logoContainer: {
-    width: 58, // Width including padding for the logo container
-    height: 58, // Height including padding for the logo container
-    borderRadius: 29, // Makes the container circular
-    borderWidth: 1, // Border thickness around the container
-    borderColor: "#F773ED", // Pink border color for the container
-    alignItems: "center", // Centers the logo horizontally in the container
-    justifyContent: "center", // Centers the logo vertically in the container
-    marginRight: 10, // Margin to separate the logo from the text
+    width: 58,
+    height: 58,
+    borderRadius: 29,
+    borderWidth: 1,
+    borderColor: "#F773ED",
+    alignItems: "center",
+    justifyContent: "center",
+    marginRight: 10,
   },
   menuHome: {
-    width: 22, // Width of the menu icon
-    height: 22, // Height of the menu icon
+    width: 22,
+    height: 22,
   },
   balanceContainer: {
-    padding: 20, // Padding around the balance section
+    padding: 20,
   },
   allCards: {
     flexDirection: "row",
@@ -181,29 +226,39 @@ const styles = StyleSheet.create({
     color: "#A0A0A0",
   },
   card: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
     width: 80,
     height: 50,
-    padding: 15,
-    backgroundColor: "#5272F2",
+    padding: 10,
     borderRadius: 10,
-    marginBottom: 15, // Spacing between cards
+    marginBottom: 15,
+    justifyContent: "flex-end",
+    paddingRight: 20,
+    marginRight: 10,
+  },
+  cardIcon: {
+    position: "absolute",
+    top: 5,
+    right: 5,
   },
   cardAmountRow: {
     flexDirection: "row",
-    alignItems: "center",
+    alignItems: "baseline",
+    position: "absolute",
+    bottom: 5,
+    left: 5,
   },
   cardAmount: {
-    fontSize: 10,
+    fontSize: 12,
+    color: "#FFFFFF",
   },
   cardAmountDecimal: {
-    fontSize: 6,
+    fontSize: 8,
+    color: "#FFFFFF",
   },
   cash: {
-    width: 12,
-    height: 12,
+    flex: 1,
+    width: 15,
+    height: 15,
   },
   paypal: {
     width: 12,
