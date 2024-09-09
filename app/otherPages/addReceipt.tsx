18fd6741f488eafc1ce8604b323ccf52f92340d6
M4rga <119053214+M4rga@users.noreply.github.com>
2024-09-09 10:38:54 +0200

    receipt v0.2

---
+++ app/otherPages/addReceipt.tsx
@@ -0,0 +1,13 @@
+import { Text } from "../../components/textFont";
+import { View } from "react-native";
+import React from "react";
+
+const AddReceipt = () => {
+  return (
+    <View>
+      <Text>AddReceipt</Text>
+    </View>
+  );
+};
+
+export default AddReceipt;
--- app/plus/receipt.tsx
+++ app/plus/receipt.tsx
@@ -1,12 +1,79 @@
 import { Text } from "../../components/textFont";
-import { View, StyleSheet, Image, TouchableOpacity } from "react-native";
-import React from "react";
+import { View, StyleSheet, Image, TouchableOpacity, Alert } from "react-native";
+import React, { useState } from "react";
+import * as ImagePicker from "expo-image-picker";
+import { useRouter } from "expo-router";
 
 const images = {
   one: require("../../assets/login/fourthImage.png"),
 };
 
+interface ImagePickerAsset {
+  uri: string;
+}
+
 const Receipt = () => {
+  const router = useRouter();
+  const [image, setImage] = useState<string | null>(null);
+
+  // Funzione per richiedere i permessi per accedere alla fotocamera
+  const askCameraPermission = async (): Promise<boolean> => {
+    const { status } = await ImagePicker.requestCameraPermissionsAsync();
+    if (status !== "granted") {
+      Alert.alert(
+        "Errore",
+        "Permessi per accedere alla fotocamera non concessi"
+      );
+      return false;
+    }
+    return true;
+  };
+
+  // Funzione per richiedere i permessi per accedere alla galleria
+  const askGalleryPermission = async (): Promise<boolean> => {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (status !== "granted") {
+      Alert.alert("Errore", "Permessi per accedere alla galleria non concessi");
+      return false;
+    }
+    return true;
+  };
+
+  // Funzione per aprire la fotocamera e scattare una foto
+  const openCamera = async () => {
+    const hasPermission = await askCameraPermission();
+    if (!hasPermission) return;
+
+    const result = await ImagePicker.launchCameraAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images, // Solo immagini
+      allowsEditing: true,
+      quality: 1, // Alta qualità
+    });
+
+    // Verifica che l'operazione non sia stata annullata e che ci siano asset validi
+    if (!result.canceled && result.assets && result.assets.length > 0) {
+      setImage(result.assets[0].uri); // Setta l'URI della foto
+      router.push("otherPages/addReceipt");
+    }
+  };
+
+  // Funzione per aprire la galleria e selezionare una foto
+  const openGallery = async () => {
+    const hasPermission = await askGalleryPermission();
+    if (!hasPermission) return;
+
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images, // Solo immagini
+      allowsEditing: true,
+      quality: 1, // Alta qualità
+    });
+
+    // Verifica che l'operazione non sia stata annullata e che ci siano asset validi
+    if (!result.canceled && result.assets && result.assets.length > 0) {
+      setImage(result.assets[0].uri); // Setta l'URI dell'immagine selezionata
+      router.push("otherPages/addReceipt");
+    }
+  };
   return (
     <View style={styles.container}>
       <Text style={styles.textStyle}>
@@ -17,10 +84,10 @@ const Receipt = () => {
         <Image source={images.one} style={styles.imageStyle} />
       </View>
       <View style={styles.viewStyle}>
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity style={styles.button} onPress={openCamera}>
           <Text style={styles.buttonText}>Scatta foto</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity style={styles.button} onPress={openGallery}>
           <Text style={styles.buttonText}>Importa</Text>
         </TouchableOpacity>
       </View>
