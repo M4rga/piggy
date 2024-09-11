a039ce53cbbf231e126ce16d221cd98c3cb09b9e
JacopoNicolini <jacopo.nicolini05@gmail.com>
2024-09-11 10:58:39 +0200

    V0.3 userProfile

--- app/otherPages/userProfile.tsx
+++ app/otherPages/userProfile.tsx
@@ -3,169 +3,149 @@ import { View, StyleSheet, ScrollView, Pressable } from "react-native"; // Impor
 import IconFeather from "react-native-vector-icons/Feather";
 import React, { useState } from "react";
 import DropdownButton from "../../components/dropdown";
+import { useRouter } from "expo-router";
 
 const currencies = ["EUR", "USD", "JPY", "GBP", "AUD", "CAD", "CHF"];
 
-
 const UserProfile = () => {
-    const [selectedTransactionType, setSelectedTransactionType] =
-        useState<string>("uscite");
-    
-    const handleDropdownSelection = (value: string) => {
-        setSelectedTransactionType(value); // Updates the item in the dropdown
-      };
-    return (
+    const router = useRouter();
+
+    const [selectedTransactionType, setSelectedTransactionType] = useState<string>("uscite");
+
+  const handleDropdownSelection = (value: string) => {
+    setSelectedTransactionType(value); // Updates the item in the dropdown
+  };
+
+  return (
     <ScrollView style={styles.container}>
       <View style={styles.section}>
         <Text style={styles.sectionTitle}>Generali</Text>
         <View style={styles.allOptions}>
-            <View>
-                <Pressable style={styles.option}>
-                    <IconFeather name="award" size={18.5} />
-                    <Text style={styles.optionLabel}>Obiettivo</Text>
-                </Pressable>
-            </View>
-            <View>
-                <Pressable style={styles.option}>
-                    <IconFeather name="dollar-sign" size={18.5} />
-                    <Text style={styles.optionLabel}>Valuta</Text>
-                    <View>
-                        <DropdownButton
-                            selectedValue={selectedTransactionType}
-                            onSelect={handleDropdownSelection}
-                            type="income/outcome/loan"
-                            color="black"
-                        />
-                    </View>
-                </Pressable>
-            </View>
-            {/* <Picker
-                selectedValue={selectedCurrency}
-                style={styles.picker}
-                onValueChange={(itemValue) => setSelectedCurrency(itemValue)}
-            >
-                {currencies.map((currency) => (
-                <Picker.Item key={currency} label={currency} value={currency} />
-                ))}
-            </Picker> */}
-            
-            <View>
-                <Pressable style={styles.option}>
-                    <IconFeather name="lock" size={18.5} />
-                    <Text style={styles.optionLabel}>Privacy</Text>
-                </Pressable>
-            </View>
+          <View>
+            <Pressable style={styles.option} onPress={() => router.push("otherPages/target")}>
+              <IconFeather name="award" size={18.5} />
+              <Text style={styles.optionLabel}>Obiettivo</Text>
+            </Pressable>
+          </View>
+          <View>
+            <Pressable style={styles.option}>
+              <IconFeather name="dollar-sign" size={18.5} />
+              <Text style={styles.optionLabel}>Valuta</Text>
+              <View style={styles.dropdownButtonContainer}>
+                <DropdownButton
+                  selectedValue={selectedTransactionType}
+                  onSelect={handleDropdownSelection}
+                  type="income/outcome/loan"
+                  color="#F9F9F9"
+                />
+              </View>
+            </Pressable>
+          </View>
+          <View>
+            <Pressable style={styles.option}>
+              <IconFeather name="lock" size={18.5} />
+              <Text style={styles.optionLabel}>Privacy</Text>
+            </Pressable>
+          </View>
         </View>
-        
       </View>
 
       <View style={styles.section}>
         <Text style={styles.sectionTitle}>Account</Text>
         <View style={styles.allOptions}>
-        <View>
-                <Pressable style={styles.option}>
-                    <IconFeather name="image" size={18.5} />
-                    <Text style={styles.optionLabel}>Foto</Text>
-                </Pressable>
-            </View>
-            <View>
-                <Pressable style={styles.option}>
-                    <IconFeather name="user" size={18.5} />
-                    <Text style={styles.optionLabel}>Nome e cognome</Text>
-                </Pressable>
-            </View>
-            <View>
-                <Pressable style={styles.option}>
-                    <IconFeather name="mail" size={18.5} />
-                    <Text style={styles.optionLabel}>Email</Text>
-                </Pressable>
-            </View>
-            <View>
-                <Pressable style={styles.option}>
-                    <IconFeather name="key" size={18.5} />
-                    <Text style={styles.optionLabel}>Password</Text>
-                </Pressable>
-            </View>
-            <View>
-                <Pressable style={styles.option}>
-                    <IconFeather name="log-out" size={18.5} />
-                    <Text style={styles.optionLabel}>Esci</Text>
-                </Pressable>
-            </View>
-            <View>
-                <Pressable style={styles.option}>
-                    <IconFeather name="x-square" size={18.5} />
-                    <Text style={styles.optionLabel}>Elimina account</Text>
-                </Pressable>
-            </View>
+          <View>
+            <Pressable style={styles.option}>
+              <IconFeather name="image" size={18.5} />
+              <Text style={styles.optionLabel}>Foto</Text>
+            </Pressable>
+          </View>
+          <View>
+            <Pressable style={styles.option}>
+              <IconFeather name="user" size={18.5} />
+              <Text style={styles.optionLabel}>Nome e cognome</Text>
+            </Pressable>
+          </View>
         </View>
       </View>
 
       <View style={styles.section}>
         <Text style={styles.sectionTitle}>Help</Text>
         <View style={styles.allOptions}>
-        <View>
-                <Pressable style={styles.option}>
-                    <IconFeather name="compass" size={18.5} />
-                    <Text style={styles.optionLabel}>Istruzioni</Text>
-                </Pressable>
-            </View>
-            <View>
-                <Pressable style={styles.option}>
-                    <IconFeather name="help-circle" size={18.5} />
-                    <Text style={styles.optionLabel}>FAQ</Text>
-                </Pressable>
-            </View>
-            <View>
-                <Pressable style={styles.option}>
-                    <IconFeather name="life-buoy" size={18.5} />
-                    <Text style={styles.optionLabel}>Supporto</Text>
-                </Pressable>
-            </View>
+          <View>
+            <Pressable style={styles.option}>
+              <IconFeather name="compass" size={18.5} />
+              <Text style={styles.optionLabel}>Istruzioni</Text>
+            </Pressable>
+          </View>
+          <View>
+            <Pressable style={styles.option}>
+              <IconFeather name="help-circle" size={18.5} />
+              <Text style={styles.optionLabel}>FAQ</Text>
+            </Pressable>
+          </View>
+          <View>
+            <Pressable style={styles.option}>
+              <IconFeather name="life-buoy" size={18.5} />
+              <Text style={styles.optionLabel}>Supporto</Text>
+            </Pressable>
+          </View>
         </View>
       </View>
     </ScrollView>
-  )
-}
+  );
+};
+
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#FCF6FB",
-        padding: 20,
-      },
-      section: {
-        marginBottom: 30,
-      },
-      sectionTitle: {
-        fontSize: 18,
-        fontFamily: "Switzer-Semibold",
-        marginBottom: 10,
-        marginLeft: 20,
-      },
-      allOptions: {
-        backgroundColor: "#FFFFFF",
-        borderRadius: 10,
-      },
-      option: {
-        flexDirection: "row",
-        // justifyContent: "space-between",
-        alignItems: "center",
-        height: 60,
-        padding: 20,
-        borderBottomWidth: 1,
-        borderBottomColor: "#FCF6FB",
-        fontFamily: "Switzer-Variable",
-      },
-      optionLabel: {
-        fontSize: 16,
-        marginLeft: 15,
-      },
-      picker: {
-        height: 50,
-        width: 120,
-      },
-      Pressable: {
-        height: 40,
-      },
+  container: {
+    flex: 1,
+    backgroundColor: "#FCF6FB",
+    padding: 20,
+  },
+  section: {
+    marginBottom: 30,
+  },
+  sectionTitle: {
+    fontSize: 18,
+    fontFamily: "Switzer-Semibold",
+    marginBottom: 10,
+    marginLeft: 20,
+  },
+  allOptions: {
+    backgroundColor: "#FFFFFF",
+    borderRadius: 10,
+  },
+  option: {
+    flexDirection: "row",
+    alignItems: "center",
+    height: 60,
+    padding: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: "#FCF6FB",
+    fontFamily: "Switzer-Variable",
+  },
+  optionLabel: {
+    fontSize: 16,
+    marginLeft: 15,
+  },
+  picker: {
+    height: 50,
+    width: 120,
+  },
+  Pressable: {
+    height: 40,
+  },
+  dropdownButtonContainer: {
+    marginLeft: "auto",
+    width: 100,
+    height: 35,
+    backgroundColor: "#F9F9F9", // Background color for better visibility
+    borderRadius: 20, // Rounded corners
+    shadowColor: "#000", // Shadow color
+    shadowOffset: { width: 0, height: 1 }, // Shadow offset for iOS
+    shadowOpacity: 0.2, // Shadow opacity for iOS
+    shadowRadius: 4, // Shadow radius for iOS
+    elevation: 2.5, // Shadow for Android
+  },
 });
-export default UserProfile
+
+export default UserProfile;
