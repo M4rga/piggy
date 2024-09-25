1520c91f2f8e37b7dc9220dd2e8fd7a7850b1db8
M4rga <119053214+M4rga@users.noreply.github.com>
2024-09-25 12:09:25 +0200

    fix

--- components/balanceTextInput.tsx
+++ components/balanceTextInput.tsx
@@ -21,7 +21,7 @@ const BalanceTextInput: React.FC<BalanceTextInputProps> = ({
 
   const handleTextChange = (input: string) => {
     // Rimuove tutte le virgole e mantiene solo il primo punto
-    const sanitizedInput = input.replace(/,/g, "").replace(/(\..*)\./g, "$1");
+    const sanitizedInput = input.replace(/,/g, ".").replace(/(\..*)\./g, "$1");
 
     setText(sanitizedInput);
 
@@ -51,7 +51,7 @@ const BalanceTextInput: React.FC<BalanceTextInputProps> = ({
             color: "black",
             fontFamily: textFont,
           }}
-          keyboardType="number-pad"
+          keyboardType="numeric"
           onChangeText={handleTextChange}
           value={text}
           onFocus={() => setIsFocused(true)}
